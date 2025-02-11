41dd40c48ce9b440b1e52d4ffefdd7ac8bd8645d
Luryann A. Cervi <92046725+LuryannC@users.noreply.github.com>
2025-02-10 22:13:36 -0300

    Unreal Engine files

---
+++ Source/HuntersGame/HuntersGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HuntersGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HuntersGame, "HuntersGame" );
